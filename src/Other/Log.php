5bf0ebe037ea88bdff82402132efda2ba34b5d26
Raghu Veer Dendukuri <raghuveer.dendukuri@gmail.com>
2022-02-26 18:47:35 +0530

    Update Log.php

--- src/Other/Log.php
+++ src/Other/Log.php
@@ -11,23 +11,58 @@ use \Monolog\Handler\FirePHPHandler;
 
 /**
  * Log Class
- *
+ * Sample Usage: 
+ * Log::channel($container, 'single')->info('Something happened!');
  */
  
 class Log
 {
-	protected $container;
-	protected $config;
+	protected static $container;
+	protected static $config;
+	protected static $logger;
 	protected $response;
 		
 	/**
 	 * Write Log to specific channel
 	 *
 	 */
-	public static function channel(Container $container, $message)
+	public static function channel(Container $container, $channel)
 	{
-            
+        self::$container = $container;
+		self::$config = self::$container->get('config');
+		
+		foreach (self::$config as $key => $value)
+		{
+			if ($key == "logging") {
+				
+				foreach ($value as $key1 => $value1)
+				{
+					if ($key1 == "channels") {
+						
+						foreach ($value1 as $key2 => $value2)
+						{
+							
+							if ($key2 == $channel) {
+								
+								// Create some handlers
+								$stream = new StreamHandler($value2["path"], Logger::DEBUG);
+								$firephp = new FirePHPHandler();
 
+								// Create the main logger of the app
+								self::$logger = new Logger($channel);
+								self::$logger->pushHandler($stream);
+								self::$logger->pushHandler($firephp);
+								
+							}
+						}
+						
+					}
+				}
+				
+			}
+		} 
+		
+		return new self;	
             
 	}
 	
@@ -35,54 +70,127 @@ class Log
 	 * Write Log to on-demand channel stack
 	 *
 	 */
-	public static function stack()
+	/* public static function stack()
 	{
             
-
+		self::$container = $container;
+		self::$config = self::$container->get('config');
+		
+		foreach (self::$config as $key => $value)
+		{
+			if ($key == "logging") {
+				
+				foreach ($value as $key1 => $value1)
+				{
+					if ($key1 == "channels") {
+						
+						foreach ($value1 as $key2 => $value2)
+						{
+							if (($key2 == "stack") && ($key2 == $channel)) {
+								
+								foreach ($value2 as $key3 => $value3)
+								{
+									if ($key3 == "channels") {
+										
+										var_dump($value3);
+										
+									}
+								}
+								
+							}
+						}
+						
+					}
+				}
+				
+			}
+		} 
+		
+		return new self;
             
-	}
+	} */
 	
 	/**
 	 * Write Log to on-demand channel configuration at runtime
 	 *
 	 */
-	public static function build()
+	/* public static function build()
 	{
             
 
             
+	} */
+	
+	/**
+	 * Writes message to logging destination
+	 *
+	 */
+	public static function debug($message)
+	{
+		self::$logger->debug($message);
 	}
 	
 	/**
-	 * Accepts Extracted Config Array
+	 * Writes message to logging destination
 	 *
-	 * @param array $configArray
-	 * @return array
 	 */
-	public static function info(Container $container, $message)
+	public static function info($message)
 	{
-		$this->container = $container;
-		
-		$this->config = $this->container->get('config');
-		
-		
-		//$this->createMonologObject();
-		
-		
-		return $this->config;
+		self::$logger->info($message);
 	}
 	
 	/**
-	 * Create Monolog Object
+	 * Writes message to logging destination
 	 *
 	 */
-	public function createMonologObject()
+	public static function notice($message)
 	{
-            
-
-            
+		self::$logger->notice($message);
 	}
 	
+	/**
+	 * Writes message to logging destination
+	 *
+	 */
+	public static function warning($message)
+	{
+		self::$logger->warning($message);
+	}
 	
+	/**
+	 * Writes message to logging destination
+	 *
+	 */
+	public static function error($message)
+	{
+		self::$logger->error($message);
+	}
+	
+	/**
+	 * Writes message to logging destination
+	 *
+	 */
+	public static function critical($message)
+	{
+		self::$logger->critical($message);
+	}
+	
+	/**
+	 * Writes message to logging destination
+	 *
+	 */
+	public static function alert($message)
+	{
+		self::$logger->alert($message);
+	}
+	
+	/**
+	 * Writes message to logging destination
+	 *
+	 */
+	public static function emergency($message)
+	{
+		self::$logger->emergency($message);
+	}
 			
 }
